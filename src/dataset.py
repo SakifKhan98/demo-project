04f7873b2a9741a2314d92b0e68cafed4d61e8a0
SakifKhan98 <sakifkhan98@gmail.com>
2024-08-30 19:06:36 -0500

    chore: Add dataset.py and notebook.ipynb

---
+++ src/dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "name": ["John", "Anna", "Peter", "Linda"],
+    "age": [23, 36, 28, 45],
+    "city": ["New York", "Paris", "Berlin", "London"],
+}
+
+df = pd.DataFrame(data)
+df
